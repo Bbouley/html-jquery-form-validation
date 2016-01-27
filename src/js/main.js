fd4cf962cb5ca6d964b198879522349da460bf24
bbouley <b.bouley@hotmail.co.uk>
2016-01-27 13:13:48 -0700

    added in jquery logic to validate

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,45 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+    });
+
+
+    ['preferredName', 'lastName'].forEach(function(inputName) {
+        var $input = $("input[name='" + inputName + "']")
+        $input.on('focusout', function() {
+            var val = $input.val();
+            if (!val) {
+                $input.addClass('warning');
+            } else {
+                $input.removeClass('warning');
+            }
+        });
+    });
+
+    var $emailInput = $('input[name="email"]')
+
+    $emailInput.on('keydown', function() {
+        var email = $emailInput.val();
+        if (!validateEmail(email)) {
+            $emailInput.addClass('warning');
+        } else {
+            $emailInput.removeClass('warning');
+        }
+    })
+
 });
+
+function validateEmail (email) {
+    if (!(email.includes('@') && email.includes('.'))) {
+        return false;
+    } else if ((email.indexOf('@')+1) === '.') {
+        return false;
+    } else if ((email.indexOf('.')+1) === '') {
+        return false;
+    } else {
+        return true;
+    }
+}
